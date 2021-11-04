d369d69c793d19e3f3fe0993d0c0b18c8554eb3c
Dmitry Khilko <dmitry.jilko@gmail.com>
2021-11-04 15:39:41 +0100

    'angular-crush' DONE

--- src/app/components/add-task/add-task.component.ts
+++ src/app/components/add-task/add-task.component.ts
@@ -1,5 +1,7 @@
+import { UiService } from 'src/app/services/ui.service';
 import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 import { Task } from 'src/app/Task';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-add-task',
@@ -10,9 +12,14 @@ export class AddTaskComponent implements OnInit {
   text!: string;
   day!: string;
   reminder: boolean = false;
+  showAddTask!:boolean;
   @Output() onAddTask:EventEmitter<Task>=new EventEmitter();
+  subscription: Subscription;
 
-  constructor() {}
+  constructor(private uiService:UiService) {
+    this.subscription = this.uiService
+    .onToggle()
+    .subscribe((value) => (this.showAddTask = value));   }
 
   ngOnInit(): void {}
   onSubmit() {
